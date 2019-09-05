932f93dc730b97f60654d2e5aa7a055252a472be
robinvh85 <robinvh85@gmail.com>
2019-09-05 17:46:57 +0700

    Add admin and test

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,27 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandsTestCase(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    # This decorator is same as with patch('time.sleep') as gi: gi.return_value=None
+    @patch('time.sleep', return_value=None)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # It wait for 5 error time. At the sixth time, it return True
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -28,3 +28,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, '123456')
+
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
